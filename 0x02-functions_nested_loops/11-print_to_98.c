550ad501fcc91171b5c901b3d6c201f9b47d9b30
Meshack-Commey <commeymeshack@gmail.com>
2022-04-13 05:44:27 +0000

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - prints numbers to 98
@@ -9,82 +10,39 @@
 
 void print_to_98(int n)
 {
-	int end;
-
-	end = 98;
-
-	if (n < end)
+	
+	if (n < 98)
 	{
-		for (; n <= end; n++)
+		while (n <= 98)
 		{
-			if (n == 98)
+			_putchar("%d", n)
+
+			if (n != 98)
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				break;
+				_putchar(", ");
+				
 			}
-			else
-			{
-				if (n >= -9 && n <= 9)
-				{
-					if (n < 0)
-					{
-						_putchar('-');
-						_putchar((n * -1) + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-					else if (n >= 0)
-					{
-						_putchar(n + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else if (n >= 100)
-				{
-					_putchar((n / 100) + '0');
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
+			n++;
 		}
-		_putchar('\n');
 	}
-	else if (n == 0)
+	else if (n > 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar('\n');
+		while (n >= 98)
+		{
+			_putchar("%d", n)
+			
+			if (n != 98)
+			{
+				_putchar(',');
+			}
+			n--;
+		}
 	}
 	else
 	{
-		for (; n >= end; n--)
-		{
-			if (n == 98)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				break;
-			}
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
+		_putchar("98")
 	}
+	_putchar("\n")
+		
 	return;
 }
