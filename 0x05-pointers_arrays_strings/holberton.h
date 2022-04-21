8b4db7adad3684c9ed439c060a2f193a88d566d8
Meshack-Commey <commeymeshack@gmail.com>
2022-04-21 05:09:25 +0000

    C Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -14,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif
